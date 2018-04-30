298a4516e4f8456dac4e00df5c2ec6220c569818
Samuel Martins Miné <samuelmmine@gmail.com>
2018-04-30 09:19:27 -0300

    Arquivos iniciais

---
+++ src/main/java/br/com/a7tecnologia/A7tecnologiaApplication.java
@@ -0,0 +1,12 @@
+package br.com.a7tecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A7tecnologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A7tecnologiaApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/a7tecnologia/A7tecnologiaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.a7tecnologia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A7tecnologiaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
